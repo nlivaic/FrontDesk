7cf81608b5aa213ecf76a652d27dd1b43f3aaeea
nlivaic <nlivaic@gmail.com>
2018-06-16 12:43:04 +0200

    feat: Initial migrations.

---
+++ Scheduling.Infrastructure/Migrations/20180614204329_Initial.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Scheduling.Infrastructure.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "schedule",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false),
+                    ClinicId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_schedule", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "appointment",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false),
+                    AppointmentTypeId = table.Column<int>(type: "int", nullable: false),
+                    ClientId = table.Column<int>(type: "int", nullable: false),
+                    DateTimeConfirmed = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    DoctorId = table.Column<int>(type: "int", nullable: false),
+                    PatientId = table.Column<int>(type: "int", nullable: false),
+                    RoomId = table.Column<int>(type: "int", nullable: false),
+                    ScheduleId = table.Column<Guid>(type: "char(36)", nullable: false),
+                    Title = table.Column<string>(type: "longtext", nullable: true),
+                    TimeRange_EndDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    TimeRange_StartDate = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_appointment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_appointment_schedule_ScheduleId",
+                        column: x => x.ScheduleId,
+                        principalTable: "schedule",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_appointment_ScheduleId",
+                table: "appointment",
+                column: "ScheduleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "appointment");
+
+            migrationBuilder.DropTable(
+                name: "schedule");
+        }
+    }
+}

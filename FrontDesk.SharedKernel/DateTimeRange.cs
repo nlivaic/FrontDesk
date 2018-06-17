730a562d8a52b5f7ca35201df43c2947155d638b
nlivaic <nlivaic@gmail.com>
2018-06-17 14:10:58 +0200

    fix: Date invariant.

--- FrontDesk.SharedKernel/DateTimeRange.cs
+++ FrontDesk.SharedKernel/DateTimeRange.cs
@@ -6,20 +6,23 @@ namespace FrontDesk.SharedKernel {
         public DateTime EndDate { get; private set; }
         
         public DateTimeRange(DateTime startDate, DateTime endDate) {
-            if (startDate < endDate)
+            if (startDate >= endDate)
             {
                 throw new ArgumentException("Start date should be earlier than end date.");
             }
             this.StartDate = startDate;
             this.EndDate = endDate;
         }
-
+        
         public DateTimeRange(DateTime startDate, TimeSpan timeLength)
         {
             this.StartDate = startDate;
             this.EndDate = startDate.Add(timeLength);
         }
 
+        /// <summary>
+        /// Needed by EF Core.
+        /// </summary>
         protected DateTimeRange() { }
     }
 }

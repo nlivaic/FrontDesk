523db27a2d782d903a59b5ba986affc171a9e2cf
nlivaic <nlivaic@gmail.com>
2018-07-29 13:21:51 +0200

    feat: Message Queue publisher.

---
+++ Scheduling.Infrastructure/ServiceBrokerMessagePublisher.cs
@@ -0,0 +1,19 @@
+using FrontDesk.SharedKernel.Interfaces;
+using Scheduling.Infrastructure.Services.Interfaces;
+
+namespace Scheduling.Infrastructure
+{
+    public class ServiceBrokerMessagePublisher : IMessagePublisher
+    {
+        public ServiceBrokerMessagePublisher()
+        {
+            #warning List dependencies needed to send the applicationEvent to some messaging queue.
+        }
+
+        public void Publish(IApplicationEvent applicationEvent)
+        {
+            #warning Implement sending applicationEvent to a messaging queue.
+            #warning Send event to queue as JSON.
+        }
+    }
+}

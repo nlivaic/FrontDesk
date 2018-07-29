9ce780bf36f793bf200399db0ce3ab1db76f4886
nlivaic <nlivaic@gmail.com>
2018-07-29 12:34:00 +0200

    docs: Removed warning.

--- Scheduling.Infrastructure/Interfaces/IAppointmentDTORepository.cs
+++ Scheduling.Infrastructure/Interfaces/IAppointmentDTORepository.cs
@@ -3,7 +3,6 @@ using Scheduling.Infrastructure.Services;
 
 namespace Scheduling.Infrastructure.Services.Interfaces
 {
-    #warning Create an implementation.
     public interface IAppointmentDTORepository
     {
         AppointmentDTO GetFromAppointment(Appointment appointment);
